acf817c177cf8c125197be68cefda15630399121
Gabriel Santiago <sheriffhermoso@gmail.com>
2021-03-03 10:31:31 -0300

    Commit Moveit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import '../styles/global.css';
+import { useState } from 'react';
+import { CountdownProvider } from '../contexts/CountdownContext';
+
+function MyApp({ Component, pageProps }) {
+
+ 
+
+
+
+  return (
+  
+        
+         <Component {...pageProps} />
+         
+ 
+
+  ) 
+}
+
+export default MyApp
+
